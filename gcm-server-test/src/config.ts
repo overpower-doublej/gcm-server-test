61dd23d88e2370fa4168296e41a7c4eca69237a9
tae-jun <i2r.jun@gmail.com>
2014-11-22 22:48:34 +0900

    Initial Commit
    simple server and gcm test

---
+++ gcm-server-test/src/config.ts
@@ -0,0 +1,5 @@
+﻿var config = {
+    port: 3000
+}
+
+export = config;
---
+++ gcm-server-test/src/route/index.ts
@@ -0,0 +1,10 @@
+﻿import express = require('express');
+
+var router = express.Router();
+router.get('/', function (req, res) {
+    res.json({
+        msg: 'Hello Android!'
+    });
+});
+
+export = router;
